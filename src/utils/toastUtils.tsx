ca5ff8599e9a412f08bad796102dd476dc39f1e2
Upadhyay <gaganupadhyay.karan@gmail.com>
2025-09-12 09:35:22 +0530

    toast now take timimgs as per requirements

--- src/utils/toastUtils.tsx
+++ src/utils/toastUtils.tsx
@@ -1,15 +1,15 @@
 import {toast} from 'react-toastify';
 
-export const toastShowSuccess = (msg: string) =>
-  toast.success(msg, { position: "top-right", autoClose: 300, theme: "colored" });
+export const toastShowSuccess = (msg: string, time:number) =>
+  toast.success(msg, { position: "top-right", autoClose: time, theme: "colored" });
 
-export const toastShowError = (msg: string) =>
-  toast.error(msg, { position: "top-right", autoClose: 300, theme: "colored" });
+export const toastShowError = (msg: string, time:number) =>
+  toast.error(msg, { position: "top-right", autoClose: time, theme: "colored" });
 
-export const toastShowLoading = (msg: string) => {
-  toast.loading(msg, {position:"top-right", autoClose:300, theme:"colored"});
+export const toastShowLoading = (msg: string, time:number) => {
+  toast.loading(msg, {position:"top-right", autoClose:time, theme:"colored"});
 }
 
-export const toastShowWarning=(msg:string)=>{
-  toast.warn(msg, {position:"top-right", autoClose:300, theme:"colored"});
+export const toastShowWarning=(msg:string, time:number)=>{
+  toast.warn(msg, {position:"top-right", autoClose:time, theme:"colored"});
 }
